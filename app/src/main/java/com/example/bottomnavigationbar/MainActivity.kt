383d3befe7d6d756ae89990cd4c6161c08095e6d
AngelTravieso <angeltraviesoc@gmail.com>
2024-02-08 18:54:55 -0400

    ADD - background img and logo

--- app/src/main/java/com/example/bottomnavigationbar/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationbar/MainActivity.kt
@@ -3,27 +3,28 @@ package com.example.bottomnavigationbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Scaffold
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -44,7 +45,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
             val tabBarItems = listOf(
                 TabBarItem(
                     title = "Inicio",
@@ -60,55 +60,95 @@ class MainActivity : ComponentActivity() {
                 )
             )
 
-            // creating our navController
             val navController = rememberNavController()
-            var selectedItem by remember { mutableStateOf(0) }
+            var selectedItem by remember { mutableIntStateOf(0) }
 
             CustomBottomNavigationBarTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    color= Color(0xFF14C6A4),
-                    modifier = Modifier.fillMaxSize(),
+                Box(
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Scaffold { innerPadding ->
-                        Box(
-                            Modifier
-                                .fillMaxSize()
-                                .padding(
-                                    bottom = 0.dp,
-                                    start = 0.dp,
-                                    end = 0.dp
-                                )
-                                .padding(top = innerPadding.calculateTopPadding())
-                        ) {
-                            // Content of the screens in the NavHost
-                            NavHost(
-                                navController = navController,
-                                startDestination = tabBarItems[0].title
-                            ) {
-                                composable(tabBarItems[0].title) {
-                                    Text(tabBarItems[0].title)
-                                }
-                                composable(tabBarItems[1].title) {
-                                    Text(tabBarItems[1].title)
-                                }
-                                composable(tabBarItems[2].title) {
-                                    Text(tabBarItems[2].title)
+                    // Fondo
+                    Image(
+                        painter = painterResource(id = R.drawable.background_cc),
+                        contentDescription = "Imagen fondo Chinchin",
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .layout { measurable, constraints ->
+                                val placeable = measurable.measure(constraints)
+                                layout(placeable.width, placeable.height) {
+                                    placeable.place(
+                                        40, // Posición a la derecha de la pantalla
+                                        0 // Sin desplazamiento vertical
+                                    )
                                 }
                             }
+                    )
 
-                            Column(
-                                modifier = Modifier.fillMaxHeight(),
-                                verticalArrangement = Arrangement.Bottom,
-                            ) {
-                                FloatingTabView(
-                                    tabBarItems = tabBarItems,
-                                    navController = navController,
-                                    floatingMargin = 10.dp,
-                                    selectedTabIndex = selectedItem
-                                ) { index ->
-                                    selectedItem = index
+                    // ChinChin Logo
+                    Image(
+                        painterResource(id = R.drawable.chinchin_logo),
+                        contentDescription = "Chinchin Logo",
+                        modifier = Modifier
+                            .size(160.dp)
+                            .layout { measurable, constraints ->
+                                val placeable = measurable.measure(constraints)
+                                layout(placeable.width, placeable.height) {
+                                    placeable.place(
+                                        0,
+                                        -100
+                                    ) // Posiciona la imagen en la esquina superior izquierda
                                 }
+                            }
+                            .padding(16.dp) // Añadir espacio desde el borde
+                    )
+
+                    // Contenido principal
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(
+                                top = 150.dp,  // Ajusta la distancia desde arriba para dejar espacio para el logotipo
+                                bottom = 0.dp,
+                                start = 0.dp,
+                                end = 0.dp
+                            )
+                    ) {
+                        // Content of the screens in the NavHost
+                        NavHost(
+                            navController = navController,
+                            startDestination = tabBarItems[0].title
+                        ) {
+                            composable(tabBarItems[0].title) {
+                                HomeViewUI()
+                            }
+                            composable(tabBarItems[1].title) {
+                                Text(tabBarItems[1].title)
+                            }
+                            composable(tabBarItems[2].title) {
+                                Text(tabBarItems[2].title)
+                            }
+                        }
+
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .layout { measurable, constraints ->
+                                    val placeable = measurable.measure(constraints)
+                                    layout(placeable.width, placeable.height) {
+                                        placeable.place(
+                                            0,
+                                            constraints.maxHeight - placeable.height
+                                        ) // Fija el FloatingTabView en el fondo
+                                    }
+                                }
+                        ) {
+                            FloatingTabView(
+                                tabBarItems = tabBarItems,
+                                navController = navController,
+                                floatingMargin = 10.dp,
+                                selectedTabIndex = selectedItem
+                            ) { index ->
+                                selectedItem = index
                             }
                         }
                     }
@@ -118,6 +158,21 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun DrawerContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text("Opción 1")
+        Spacer(modifier = Modifier.height(8.dp))
+        Text("Opción 2")
+        Spacer(modifier = Modifier.height(8.dp))
+        Text("Opción 3")
+        // Puedes agregar más elementos de menú según sea necesario
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -133,7 +188,7 @@ fun FloatingTabView(
         modifier = Modifier
             .padding(all = floatingMargin),
         shape = MaterialTheme.shapes.extraLarge, // This provides rounded edges to the Surface
-       shadowElevation = 2.dp // Adjust shadow elevation for the floating effect
+        shadowElevation = 2.dp // Adjust shadow elevation for the floating effect
     ) {
         NavigationBar(contentColor = Color(0xFF14C6A4)) {
             tabBarItems.forEachIndexed { index, item ->
@@ -144,7 +199,7 @@ fun FloatingTabView(
                             Icon(
                                 painterResource(id = item.icon),
                                 contentDescription = item.title,
-                                modifier = Modifier.size(24.dp),
+                                modifier = Modifier.size(20.dp),
                             )
 
                             Spacer(modifier = Modifier.width(4.dp))
@@ -166,10 +221,18 @@ fun FloatingTabView(
                     colors = androidx.compose.material3.NavigationBarItemDefaults
                         .colors(
                             selectedIconColor = Color(0xFF14C6A4),
-                            indicatorColor = MaterialTheme.colorScheme.surfaceColorAtElevation(LocalAbsoluteTonalElevation.current)
+                            indicatorColor = MaterialTheme.colorScheme.surfaceColorAtElevation(
+                                LocalAbsoluteTonalElevation.current
+                            )
                         )
                 )
             }
         }
     }
+}
+
+
+@Composable
+fun HomeViewUI() {
+    Text(text = "Home")
 }
