86a25e90e7b7147e1e51a37c5da7e53c7a9fd8aa
AngelTravieso <angeltraviesoc@gmail.com>
2024-02-09 12:00:22 -0400

    ADD - CollectionBox component

--- app/src/main/java/com/example/bottomnavigationbar/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationbar/MainActivity.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.*
@@ -22,12 +29,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -35,9 +49,18 @@ import androidx.navigation.compose.rememberNavController
 import com.example.bottomnavigationbar.ui.theme.CustomBottomNavigationBarTheme
 
 data class TabBarItem(
-
     val title: String,
     val icon: Int,
+)
+
+data class CollectBox(
+    val title: String,
+    val icon: Int,
+)
+
+data class PaymentBox(
+    val title: String,
+    val icon: Int
 )
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -49,12 +72,10 @@ class MainActivity : ComponentActivity() {
                 TabBarItem(
                     title = "Inicio",
                     icon = R.drawable.home,
-                ),
-                TabBarItem(
+                ), TabBarItem(
                     title = "Pagos",
                     icon = R.drawable.paymeny,
-                ),
-                TabBarItem(
+                ), TabBarItem(
                     title = "Operaciones",
                     icon = R.drawable.operations,
                 )
@@ -68,8 +89,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize()
                 ) {
                     // Fondo
-                    Image(
-                        painter = painterResource(id = R.drawable.background_cc),
+                    Image(painter = painterResource(id = R.drawable.background_cc),
                         contentDescription = "Imagen fondo Chinchin",
                         modifier = Modifier
                             .fillMaxSize()
@@ -81,21 +101,18 @@ class MainActivity : ComponentActivity() {
                                         0 // Sin desplazamiento vertical
                                     )
                                 }
-                            }
-                    )
+                            })
 
                     // ChinChin Logo
-                    Image(
-                        painterResource(id = R.drawable.chinchin_logo),
+                    Image(painterResource(id = R.drawable.chinchin_logo),
                         contentDescription = "Chinchin Logo",
                         modifier = Modifier
-                            .size(160.dp)
+                            .size(140.dp)
                             .layout { measurable, constraints ->
                                 val placeable = measurable.measure(constraints)
                                 layout(placeable.width, placeable.height) {
                                     placeable.place(
-                                        0,
-                                        -100
+                                        0, -100
                                     ) // Posiciona la imagen en la esquina superior izquierda
                                 }
                             }
@@ -107,16 +124,13 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(
-                                top = 150.dp,  // Ajusta la distancia desde arriba para dejar espacio para el logotipo
-                                bottom = 0.dp,
-                                start = 0.dp,
-                                end = 0.dp
+                                top = 70.dp,  // Ajusta la distancia desde arriba para dejar espacio para el logotipo
+                                bottom = 0.dp, start = 0.dp, end = 0.dp
                             )
                     ) {
                         // Content of the screens in the NavHost
                         NavHost(
-                            navController = navController,
-                            startDestination = tabBarItems[0].title
+                            navController = navController, startDestination = tabBarItems[0].title
                         ) {
                             composable(tabBarItems[0].title) {
                                 HomeViewUI()
@@ -129,19 +143,16 @@ class MainActivity : ComponentActivity() {
                             }
                         }
 
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .layout { measurable, constraints ->
-                                    val placeable = measurable.measure(constraints)
-                                    layout(placeable.width, placeable.height) {
-                                        placeable.place(
-                                            0,
-                                            constraints.maxHeight - placeable.height
-                                        ) // Fija el FloatingTabView en el fondo
-                                    }
+                        Box(modifier = Modifier
+                            .fillMaxWidth()
+                            .layout { measurable, constraints ->
+                                val placeable = measurable.measure(constraints)
+                                layout(placeable.width, placeable.height) {
+                                    placeable.place(
+                                        0, constraints.maxHeight - placeable.height
+                                    ) // Fija el FloatingTabView en el fondo
                                 }
-                        ) {
+                            }) {
                             FloatingTabView(
                                 tabBarItems = tabBarItems,
                                 navController = navController,
@@ -159,23 +170,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DrawerContent() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Text("Opción 1")
-        Spacer(modifier = Modifier.height(8.dp))
-        Text("Opción 2")
-        Spacer(modifier = Modifier.height(8.dp))
-        Text("Opción 3")
-        // Puedes agregar más elementos de menú según sea necesario
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
 fun FloatingTabView(
     tabBarItems: List<TabBarItem>,
     navController: NavHostController,
@@ -185,8 +179,7 @@ fun FloatingTabView(
 ) {
     // Wrapping our NavigationBar in a Surface to apply elevation.
     Surface(
-        modifier = Modifier
-            .padding(all = floatingMargin),
+        modifier = Modifier.padding(all = floatingMargin),
         shape = MaterialTheme.shapes.extraLarge, // This provides rounded edges to the Surface
         shadowElevation = 2.dp // Adjust shadow elevation for the floating effect
     ) {
@@ -194,21 +187,18 @@ fun FloatingTabView(
             tabBarItems.forEachIndexed { index, item ->
 
                 NavigationBarItem(
+                    label = {
+                        Text(text = item.title)
+                    },
                     icon = {
-                        Row {
-                            Icon(
-                                painterResource(id = item.icon),
-                                contentDescription = item.title,
-                                modifier = Modifier.size(20.dp),
-                            )
-
-                            Spacer(modifier = Modifier.width(4.dp))
-                            Text(text = item.title)
-                        }
+                        Icon(
+                            painterResource(id = item.icon),
+                            contentDescription = item.title,
+                            modifier = Modifier.size(20.dp),
+                        )
                     },
 
-                    selected = index == selectedTabIndex,
-                    onClick = {
+                    selected = index == selectedTabIndex, onClick = {
                         if (index != selectedTabIndex) {
                             onTabSelected(index)
                             navController.navigate(item.title) {
@@ -217,9 +207,7 @@ fun FloatingTabView(
                                 launchSingleTop = true
                             }
                         }
-                    },
-                    colors = androidx.compose.material3.NavigationBarItemDefaults
-                        .colors(
+                    }, colors = androidx.compose.material3.NavigationBarItemDefaults.colors(
                             selectedIconColor = Color(0xFF14C6A4),
                             indicatorColor = MaterialTheme.colorScheme.surfaceColorAtElevation(
                                 LocalAbsoluteTonalElevation.current
@@ -234,5 +222,124 @@ fun FloatingTabView(
 
 @Composable
 fun HomeViewUI() {
-    Text(text = "Home")
+    Column() {
+        Box(modifier = Modifier.padding(horizontal = 10.dp).shadow(elevation = 8.dp, shape = MaterialTheme.shapes.extraLarge)) {
+            CollectionBox()
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Box(modifier = Modifier.padding(horizontal = 10.dp).shadow(elevation = 8.dp, shape = MaterialTheme.shapes.extraLarge)) {
+            PaymentBox()
+        }
+    }
+}
+
+@Composable
+fun CollectionBox() {
+
+    val collectBoxItems = listOf(
+        CollectBox(
+            title = "Identipago",
+            icon = R.drawable.identipago
+        ),
+        CollectBox(
+            title = "Chinchin",
+            icon = R.drawable.chinchin
+        ),
+        CollectBox(
+            title = "Pago Movil",
+            icon = R.drawable.pago_movil
+        ),
+        CollectBox(
+            title = "Bancos Nacionales",
+            icon = R.drawable.bancos_nacionales
+        ),
+    )
+
+    Column(modifier = Modifier.background(Color.White)) {
+        Text(
+            text = "Recaudar",
+            fontWeight = FontWeight.W400,
+            fontSize = 16.sp,
+            modifier = Modifier.padding(top = 24.dp, start = 24.dp)
+        )
+
+        LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+            items(collectBoxItems) { item ->
+                Box(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp, vertical = 10.dp)
+                        .clip(CircleShape) // Aplica el clip de forma circular al box
+                        .background(color = Color(0xFF2D2A26))
+                        .aspectRatio(1f) // Asegura que el width y el height sean iguales para hacer el círculo
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Icon(
+                            painterResource(id = item.icon),
+                            contentDescription = item.title,
+                            tint = Color.White,
+                        )
+                        Spacer(modifier = Modifier.height(5.dp))
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 8.dp) // Agrego padding horizontal para controlar el ancho máximo
+                        ) {
+                            Text(
+                                text = item.title,
+                                color = Color.White,
+                                fontWeight = FontWeight.W500,
+                                textAlign = TextAlign.Center,
+                                maxLines = 2,
+                                overflow = TextOverflow.Ellipsis // Esto garantiza que el texto se divida en dos líneas si es necesario
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PaymentBox() {
+
+    val paymentBoxItems = listOf(
+        PaymentBox(
+            title = "movistar",
+            icon = R.drawable.movistar
+        ),
+        PaymentBox(
+            title = "movistar",
+            icon = R.drawable.movistar
+        ),
+        PaymentBox(
+            title = "SimpleTV",
+            icon = R.drawable.movistar
+        ),
+        PaymentBox(
+            title = "movistar",
+            icon = R.drawable.movistar
+        ),
+        PaymentBox(
+            title = "movistar",
+            icon = R.drawable.movistar
+        ),
+    )
+
+    Column(modifier = Modifier.background(Color.White)) {
+        Text(
+            text = "Pagos",
+            fontWeight = FontWeight.W400,
+            fontSize = 16.sp,
+            modifier = Modifier.padding(top = 24.dp, start = 24.dp)
+        )
+    }
+
 }
